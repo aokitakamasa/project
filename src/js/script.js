8b473eaf7e2eb95f80b19c844e3cfd452b9e1d47
MarinaKuvshinova <pn-05@ukr.net>
2019-11-10 01:57:32 +0200

    Class calculator and buttons relationship. Changing the structure of HTML.

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,48 @@
 class Calculator{
-    constructor(){
+    constructor(inputFunction){
         this.operation = "";
         this.currentValue = "";
         this.result = NaN;
         this.numberValue1 = NaN;
         this.numberValue2 = NaN;
+//        this.outputInput = outputInput;
+        this.outputText = "";
+        this.outputFunction = inputFunction;
     }
 
     //function addition
-    addFunction(number1, number2){
-        this.result = number1 + number2;
-        this.clearFunction();
+    addFunction(){
+        this.result = this.numberValue1 + this.numberValue2;
+        this.outputFunction(this.result);
+        this.clearAfterOperFunction();
     }
 
     //function subtraction
-    subFunction(number1, number2){
-        this.result = number1 - number2;
-        this.clearFunction();
+    subFunction(){
+        this.result = this.numberValue1 - this.numberValue2;
+        this.outputFunction(this.result);
+        this.clearAfterOperFunction();
     }
 
     //function multiplication
-    mulFunction(number1, number2){
-        this.result = number1 * number2;
-        this.clearFunction();
+    mulFunction(){
+        this.result = this.numberValue1 * this.numberValue2;
+        this.outputFunction(this.result);
+        this.clearAfterOperFunction();
     }
 
     //function division
-    divFunction(number1, number2){
-        if(!number2){
-            this.result = number1 / number2;
-            this.clearFunction();
+    divFunction(){
+        if(this.numberValue2){
+            this.result = this.numberValue1 / this.numberValue2;
+            this.outputFunction(this.result);
+            this.clearAfterOperFunction();
         }
         else{
             console.error("Division by zero is not possible");
-            this.clearFunction();
+            this.outputFunction("Error");
+            alert("Division by zero is not possible");
+            this.cleanCalcFunction();
         }
     }
 
@@ -55,51 +64,127 @@ class Calculator{
     //function cleaning after operation
     clearAfterOperFunction(){
         this.numberValue1 = this.result;
-        this.result = NaN;
         this.numberValue2 = NaN;
     }
 
     //function number formation
     numberFunction(number){
-        if(this.currentValue.length == 0 && number == 0) return;
+        if(this.currentValue.length == 0 && number == 0 && !this.numberValue1) return;
         this.currentValue += number;
+        this.outputText = this.currentValue;
+        this.outputFunction(this.outputText);
     }
 
     //function operator click
     operatorFunction(operator){
-        this.operation = operator;
         if (isNaN(this.numberValue1))
         {
             this.numberValue1 = +this.currentValue;
             this.numberValue2 = +this.currentValue;
+            this.outputText += operator;
         } else{
             this.numberValue2 = +this.currentValue;
+            this.outputText += operator;
         }
+        if(this.operation){
+            this.outputText = this.outputText.slice(0, -1); 
+            this.outputText += operator;
+        }
+        if(this.operation && this.numberValue1 && this.numberValue2)
+        {
+            this.calcFunction();
+        }
+        this.operation = operator;
         this.currentValue = "";
     }
 
     //function calculation
     calcFunction(){
-        if(operation){
-            switch(operation){
+        this.numberValue2 = +this.currentValue;
+        if(this.operation){
+            switch(this.operation){
                 case "+":
-                    addFunction(this.numberValue1, this.numberValue2);
+                    this.addFunction(this.numberValue1, this.numberValue2);
                     break;
                 case "-":
-                    addFunction(this.numberValue1, this.numberValue2);
+                    this.subFunction(this.numberValue1, this.numberValue2);
                     break;
                 case "*":
-                    addFunction(this.numberValue1, this.numberValue2);
+                    this.mulFunction(this.numberValue1, this.numberValue2);
                     break;
                 case "/":
-                    addFunction(this.numberValue1, this.numberValue2);
+                    this.divFunction(this.numberValue1, this.numberValue2);
                     break;
             }
+        }
+        else {
+            console.error("no click operation");
+            return true;
         }
     }
 }
 
+
+
+
+
 //calculator implementation
-let calc = new Calculator();
+let numberButtonList = document.getElementsByName("numberButton");
+let outputInput = document.getElementsByName("textEnter")[0].value;
+let operationButtonList = document.getElementsByName("operationButton");
+let equallyButton = document.getElementsByName("equallyButton")[0];
+
+let calc = new Calculator(inputFunction);
+
+addEventOnNumberButton(numberButtonList);
+addEventOnOperandButton(operationButtonList);
+calculationFunction(equallyButton);
+
+//function subscribing numbered buttons to a click event
+function addEventOnNumberButton(numberList){
+    if(numberList){
+        for (const numberButton of numberList) {
+            numberButton.addEventListener("click", () => {calc.numberFunction(numberButton.value);});
+        }
+    } else{
+        console.warn("no buttons with numberButton attribute name");
+    }
+}
+
+//function subscribing to click events for operand buttons
+function addEventOnOperandButton(operandList)
+{
+    if(operandList){
+        for (const operandButton of operandList) {
+            operandButton.addEventListener("click", () => {calc.operatorFunction(operandButton.value);});
+        }
+    } else{
+        console.warn("no buttons with operationButton attribute name");
+    }
+}
+
+//function expression calculation
+function calculationFunction(equallyButton){
+    if (equallyButton) {
+        equallyButton.addEventListener("click", () => {calc.calcFunction();});
+    } else{
+        console.warn("no button with equallyButton attribute name");
+    }
+}
+
+
+function inputFunction(newValue){
+    document.getElementsByName("textEnter")[0].value = newValue;
+}
+
+
+
+
+
+
+
+
+
+
 
 
